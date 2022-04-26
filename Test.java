5981fcdc04daa263439fcde8ca6bc47af085e276
pratikkundnani <46517470+pratikkundnani@users.noreply.github.com>
2022-04-27 04:07:04 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Add
+{  
+   public static void main(String args[])   
+ {  
+	int n1=Integer.parseInt(args[0]), n2=Integer.parseInt(args[1]), sum; 
+	System.out.println(n1 +" "+ n2); 
+	sum = n1 + n2;  
+	System.out.println("The sum of numbers is: "+sum);  
+ }  
+}  
+
